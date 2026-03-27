f2b5d88944d5e7911ad0fae11eaef5bf358c5976
sehajdeep10096 <sehajdeeps1313@gmail.com>
2026-03-27 21:11:15 +0530

    initial commit

---
+++ mood.py
@@ -0,0 +1,8 @@
+import pandas as pnd
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder 
+import pickle as pkl
+import random
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
